448500af668a8762ff29df366eee8339072f26a3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-27 20:06:32 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <body id="home">
+
+    <section class="hero-cover">
+      <h1>An open-source solution for cookies and GDPR</h1>
+      <p></p>
+      <button>Watch Tutorial</button>
+      <button>Download Files</button>
+    </section>
+
+    <section>
+      <h2>Watch the tutorial</h2>
+      <img src="#">
+    </section>
+
+    <section>
+      <h2>Documentation</h2>
+    </section>
+
+    <section>
+      <h2>Download</h2>
+    </section>
+
+  </body>
+</html>
