a625d0ed2272347f7285d617ddb5993177c86224
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-12-13 16:40:24 +0100

    Added GTM

--- index.php
+++ index.php
@@ -17,6 +17,11 @@
 
   <body id="home">
 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRC582D"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <div id="hello-bar">
       <small>We don't express any legal advice, this is just an open-source tech project.</small>
     </div>
@@ -30,8 +35,8 @@
       <section id="hero-cover">
         <h1>A free solution for your website's privacy protection</h1>
         <p>Privacy Protection is not just for legal compliance, it's about caring for your users.</p>
-        <button><i class="fa-solid fa-book"></i> View Documentation</button>
-        <button><i class="fa-solid fa-folder-arrow-down"></i> Download Files</button>
+        <button><i class="fa-solid fa-book"></i> Documentation</button>
+        <button><i class="fa-solid fa-circle-down"></i> Download</button>
       </section>
 
       <section id="documentation" class="container">
@@ -50,7 +55,7 @@
       <section id="download" class="container">
         <h2>Download</h2>
         <a href=""><button class="donation-button"><i class="fa-regular fa-heart"></i><i class="fa-solid fa-heart"></i> Send a Donation</button></a>
-        <button><i class="fa-solid fa-folder-arrow-down"></i> Download Files</button>
+        <button><i class="fa-solid fa-circle-down"></i> Download Files</button>
 
       </section>
 
--- sections/header.php
+++ sections/header.php
@@ -1,7 +1,7 @@
 <header>
   <img src="imgs/poly-cookies-logo.svg" height="50px" width="auto" alt="Poly Cookies">
   <nav>
-    <a href="https://github.com/The-Poly-Group" target="_blank" style="margin-right:20px;"><i class="fa-brands fa-github"></i> an open-source project</a>
+    <a id="github" href="https://github.com/The-Poly-Group" target="_blank" style="margin-right:20px;"><i class="fa-brands fa-github"></i> an open-source project</a>
     <a href=""><button class="donation-button"><i class="fa-regular fa-heart"></i><i class="fa-solid fa-heart"></i> Send a Donation</button></a>
   </nav>
 </header>
