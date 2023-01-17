f3cac6841040ab9a00a02d0cf260643f28d372e2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-01-17 03:05:58 +0100

    Added mono font to code

--- sections/head.php
+++ sections/head.php
@@ -60,5 +60,6 @@
 
   <!-- FONTS -->
   <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,500;0,600;0,700;1,800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet">
 
 </head>
