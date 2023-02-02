8e7393f66f558b5beb12478ab7a1c6ecc0f49743
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-02-02 22:05:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,9 @@
       </section>
 
       <section id="documentation" class="container">
+        <h2>Video Tutorial</h2>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QSKiPr_VgPo?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <hr>
         <h2>Installation Guide</h2>
         <div class="tasks">
           <input type="checkbox" id="task-no-1" name="task-no-1">
