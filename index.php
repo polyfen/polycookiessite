c780ca24c2faa7addcafc4a67db1c515a06d46a6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-17 02:19:37 +0200

    Updated GTM Container

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <body id="home">
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRC582D"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBRK5MR5"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
