735f99566772cf9fc7a8247c47e01a87d39b9ac5
xxiicode <leandrobilokapic@gmail.com>
2023-12-19 17:11:32 +0100

    github name changes

--- sections/header.php
+++ sections/header.php
@@ -3,7 +3,7 @@
    <img src="imgs/poly-cookies-logo.svg" alt="Poly Cookies">
   </a>
   <nav>
-    <a id="github" href="https://github.com/thepolygroup/polycookiesapp" target="_blank" style="margin-right:20px;"><i class="fa-brands fa-github"></i> an open-source project</a>
+    <a id="github" href="https://github.com/polyfen/polycookiesapp" target="_blank" style="margin-right:20px;"><i class="fa-brands fa-github"></i> an open-source project</a>
     <a href="https://donate.stripe.com/9AQ5lgeoDef2b1CdQQ" target="_blank"><button class="secondary-button donation-button"><i class="fa-regular fa-heart"></i><i class="fa-solid fa-heart"></i> Send a Donation</button></a>
   </nav>
 </header>
