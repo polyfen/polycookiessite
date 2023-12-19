694f68c71695c3cd313559456de8c5baf205f792
xxiicode <leandrobilokapic@gmail.com>
2023-12-19 17:11:32 +0100

    github name changes

--- sections/footer.php
+++ sections/footer.php
@@ -3,7 +3,7 @@
   <div id="social-icons">
     <ul>
         <li><a href="https://twitter.com/polycookies" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-        <li><a href="https://github.com/thepolygroup" target="_blank" title="Github"><i class="fab fa-github"></i></a></li>
+        <li><a href="https://github.com/polyfen" target="_blank" title="Github"><i class="fab fa-github"></i></a></li>
     </ul>
   </div>
 
