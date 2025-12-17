1203d16b04263c39f60ff52414cb902ca285d9b8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2025-12-17 17:30:27 -0300

    [COOKIES] improve: consent cookie duration

--- index.php
+++ index.php
@@ -140,9 +140,9 @@
           <code>
 &lt;script>
   var date = new Date();
-  date.setTime(date.getTime() + (90 * 24 * 60 * 60 * 1000));
+  date.setTime(date.getTime() + (180 * 24 * 60 * 60 * 1000));
   var expires = "expires=" + date.toUTCString();
-  document.cookie = 'consentAccepted=true; ' + expires + '; path=/';
+  document.cookie = 'cookiesConsentGranted=true; ' + expires + '; path=/';
 &lt;/script>
           </code>
         </pre>
